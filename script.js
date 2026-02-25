b487a7642b988c01007f806c02ff2dc6a15b0f6f
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-02-25 09:06:56 -0300

    Aula Prática com JS, conceitos Básicos

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+
 //console.log("Olá, Mundo");
+
+/*
 var a = 10; 
 var b = 0;
 console.log(a/b);
+*/
+
+/*
+const cidade = "Belo Horizonte"
+const input = "belo Horizonte"
+const inputMin = input.toLocaleLowerCase();
+console.log(cidade === inputMin);
+*/
+
+/*
+const senha = "minhaSenha1234"
+console.log(senha.length);
+*/
+
+/*
+let MinhaVar = null;
+
+if (MinhaVar === null){
+    console.log('Não a informações')
+}
+else{
+    console.log('MinhaVar')
+}
+*/
+
+let input1 = null;
+let input2;
+console.log(input1);
+console.log(input2);
+
 
