41fc8f984ace05811df08540b72feb4aba4f7d3e
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-04 09:02:55 -0300

    ok

--- aula02/script02.js
+++ aula02/script02.js
@@ -1,6 +1,49 @@
-var altura = 5;
-var comprimento = 5;
+//var altura = 5;
+//var comprimento = 5;
 
-area = altura * comprimento;
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+/*
+let  forma = 'retangulo';
+let  altura = 5;
+let  comprimento = 10;
+let  area;
+if(forma === 'retangulo'){
+    area = altura * comprimento;
+} else{
+    area = (altura * comprimento) /2;
+}
+
 console.log(area);
-var area;
+*/
+
+/*
+valor = 20 / 100
+console.log(valor);
+*/
+
+//console.log(area);
+//console.log(0 == false);
+//console.log('' == false);
+
+
+const numero = 456;
+//const numeroString = '456';
+//console.log(numero === numeroString); /* ele compara o tipo: n° e string */
+//console.log(numero == numeroString); /* ele compara o valor ou a estética */
+//console.log(numero + numeroString); /* concatenar */
+const numeroString = Number('456');
+console.log(numero + numeroString)
+
+let telefone = 12345678;
+console.log("O telefone é " + telefone.toString());
+
+
+
+
+
+
+
+
