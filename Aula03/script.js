5f25279ab1b80009b104da629e1ba618eb62ce70
Lucas Aguiar Correa <lucas.aguiar1967@gmail.com>
2026-03-15 12:22:19 -0300

    Ok

---
+++ Aula03/script.js
@@ -0,0 +1,22 @@
+function imprimeTexto(texto){
+
+}
+
+imprimeTexto("Oi");
+imprimeTexto(soma());
+
+function soma(){
+    return 2 + 2; 
+}
+
+
+function soma (numero1, numero2){
+    return numero1 + numero2;
+}
+
+
+function Multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(Multiplica(soma(4,5), soma(3,3)));
