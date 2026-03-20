5c0c75879dfa430f3b2ae095bfd682229e9017fc
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-20 09:34:55 -0300

    ok

--- aula06/script.js
+++ aula06/script.js
@@ -15,6 +15,7 @@ console.log(arrayVazia);
 console.log(arrayVazia[0]);
 console.log(arrayVazia[1]);
 console.log(arrayVazia[2]);
+
 arrayVazia.push(50);
 console.log(arrayVazia);
 console.log(arrayVazia.length);
