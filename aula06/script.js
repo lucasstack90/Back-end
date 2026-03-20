a0b4cf8e04097a84b8b9dbbd23cc1a83507ad232
Lucas Aguiar <lucas.aguiar1967@gmail.com>
2026-03-20 09:26:55 -0300

    Aula de hoje

---
+++ aula06/script.js
@@ -0,0 +1,22 @@
+const notas=[10, 6.5, 8, 7.5];
+
+const media = (nota[0] + nota[1] + nota[2] + nota[3])/ notas.length;
+
+console.log(media); 
+console.log(notas[3]);
+notas.push(9);
+console.log(notas);
+notas.pop();
+console.log(notas);
+
+const arrayVazia = [, , ,]
+
+console.log(arrayVazia);
+console.log(arrayVazia[0]);
+console.log(arrayVazia[1]);
+console.log(arrayVazia[2]);
+
+
+
+
+
